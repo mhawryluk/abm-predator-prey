3a7d80d6e91a1be6426ea0754adf6d7eaf53f46b
mhawryluk <marcinhawrylukxx@gmail.com>
2023-12-02 22:41:44 +0100

    Add plotting population counts

--- model.py
+++ model.py
@@ -2,14 +2,16 @@ from random import random, choice, randint
 import pygame
 from pygame.locals import (
     K_RIGHT,
-    K_ESCAPE,
     KEYDOWN,
     QUIT,
 )
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Model:
     def __init__(self, width, height):
+        self.simulationDay = 0
         self.width = width
         self.height = height
         self.worldGrid = [
@@ -24,10 +26,18 @@ class Model:
         
         for _ in range(20):
             self.preys.add(Prey(randint(0, self.width-1), randint(0, self.height-1), self.worldGrid))
+    
+    def getPredatorCount(self):
+        return len(self.predators)
+
+    def getPreyCount(self):
+        return len(self.preys)
 
 
     def step(self):
         # todo: asynchronous
+        self.simulationDay += 1
+        
         for predator in list(self.predators):
             alive, reproduced = predator.step()
             if not alive:
@@ -267,37 +277,73 @@ class Prey(Animal):
 
 if __name__ == '__main__':
     pygame.init()
+    pygame.display.set_caption('Predator-Prey Agent Based Simulation')
+    
     windowWidth = 800
     windowHeight = 800
+    
     screen = pygame.display.set_mode([windowWidth, windowHeight])
-
-    # Create Board object
-    model = Model(100, 100)
-    # Set background
     screen.fill((255, 255, 255))
 
-    # Draw grid
+    model = Model(100, 100)
     model.draw(screen, windowWidth, windowHeight)
-        
-    running = True
 
     time_delay = 200 # 0.2 s
     timer_event = pygame.USEREVENT + 1
-    pygame.time.set_timer(timer_event, time_delay )
+    pygame.time.set_timer(timer_event, time_delay)
+    
+    plt.ion()
+    
+    predatorCounts = []
+    preyCounts = []
+    predatorGraph = plt.plot(predatorCounts, color='#e63946', linewidth=3, label='Predators')[0]
+    preyGraph = plt.plot(preyCounts, color='#4361ee', linewidth=3, label='Prey')[0]
+    
+    xLim = 100
+    yLim = 100   
+    plt.xlim([0, xLim])
+    plt.ylim([0, yLim])
+    
+    plt.xlabel('day')
+    plt.ylabel('count')
+    plt.title('Population counts')
+    ax = plt.gca()
+    ax.set_facecolor('#fefae0')
+    plt.legend()
+    
+    running = True
 
     while running:
+        
         for event in pygame.event.get():   
             if event.type == QUIT:
                 running = False
             
-            if event.type == KEYDOWN:
-                if event.key == K_RIGHT:
+            if event.type == timer_event or (event.type == KEYDOWN and event.key == K_RIGHT):
                     model.step()
+                    
+                    predatorCount = model.getPredatorCount()
+                    predatorCounts.append(predatorCount)
+                    
+                    preyCount = model.getPreyCount()
+                    preyCounts.append(preyCount)
+                    
+                    day = model.simulationDay
+                    
+                    if day > xLim:
+                        xLim = 2*day
+                        plt.xlim([0, xLim])
+                    
+                    if predatorCount > yLim:
+                        yLim = 2*predatorCount
+                        plt.ylim([0, yLim])
+                    
+                    if preyCount > yLim:
+                        yLim = 2*preyCount
+                        plt.ylim([0, yLim])
+                    
                     model.draw(screen, windowWidth, windowHeight)
-
-                
-            if event.type == timer_event:
-                model.step()
-                model.draw(screen, windowWidth, windowHeight)
-
+                    predatorGraph.set_data(list(range(day)), predatorCounts)
+                    preyGraph.set_data(list(range(day)), preyCounts)
+                    
     pygame.quit()
